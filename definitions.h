f26d5fd46a23680bba61896fb795a1ffdf047c6d
Neunis <n_nunis@u.pacific.edu>
2019-12-19 17:19:46 -0600

    Update Definitions
    -Data log updated with strings for messages when water pump status changes
    -pin-outs updated for each tank error
    -pin-outs updated/changed because of micro-controller change
    -times defined for general and manual automation of water pumps
    -times defined for when pump starts and stops

--- definitions.h
+++ definitions.h
@@ -10,8 +10,8 @@
 #define manualOveride 23
 
 #define tank4Clean 24
-#define tank4Low 27
-#define tank4High 26
+#define tank4Low 12
+#define tank4High 11
 
 #define tank3Clean 10
 #define tank3Low 9
@@ -25,9 +25,32 @@
 #define tank1Low 3
 #define tank1High 2
 
+//each tanks error pin
+#define tank1Error 14
+#define tank2Error 15
+#define tank3Error 16
+#define tank4Error 17
+
 #define chipSelect BUILTIN_SDCARD
 //MISO,sClock, MOSI are already defined, do nothing
 
-
 //Single Tank test
 #define pump 13
+
+//Time defintions for when pump is active
+#define timepumpStartsHr 10
+#define timepumpStopsHr 16
+#define howlongAutoPumpRanSec 3600
+#define howlongManPumpRanSec 500
+
+String message = "Pump has been turned off";;
+String startPumpMessage = "Automated pump has started";
+String eShutoffMessage = "Emergency Shutoff switch has been intiated";
+String cisternMessage = "Cistern is low, pump disabled.";
+String manualOverideMessage = "Manual override has been engaged";
+String manualPumpDailyRun = "Manual Pump has been run for the day";
+String pumpOffMessage = "Pump has been turned off";
+String pumpDailyRun = "Pump has run for the day";
+String cleaningStatus = "Pump is being cleaned";
+String errorFloatStatus = "High float switch is closed, low level float switch is open";
+String Neutral = "Pump is off, cistern level is met, tanks are full, or time allotted has been met";
