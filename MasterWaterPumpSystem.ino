fd996a92e8a9a942b9a06d11d271e7bacfb1061a
curamoto <58994372+curamoto@users.noreply.github.com>
2020-01-14 13:16:22 -0800

    Final Code Update
    Final code update
    
    For automated pump control system:
    Main code includes being set to non-test time parameters
    
    Error handling for tanks:
    If all tanks are cleaning don’t pump
    If the system has run for the days allotted time don’t pump
    Datalogging for everything implemented (Error, Cleaning, Pump state, and EOC [eShutoff, manual override, cistern condition])
    
    Task accomplished for tanks:
    Time variables adjusted for testing vs daily application
    LEDs and functionality applied for each tank
    Time resets for each day and based on the mode manual or automatic
    LED error light is reset when there is no longer an error
    pumpLED signal was changed to pumpControl to allow the system to toggle the surge protector on or off
    
    @Neunis

--- MasterWaterPumpSystem.ino
+++ MasterWaterPumpSystem.ino
@@ -3,6 +3,9 @@
 #include <SPI.h>
 #include "definitions.h"
 
+//TODO: errorRecord[respective tank num] that is set equal to one needs
+//to be moved after the print out statement when fxn is called to avoid multiple print outs
+
 //  * - can be taken out of final code
 //  t - needs to be reworked
 
@@ -15,18 +18,32 @@ int currentDay;
 time_t timeStart; //When we start pumping the tank
 time_t manualTimeStart; //separate time var to address when manualOverride occurs
 
-
 void setup() {
 
   /**/
   pinMode (tank1Low, INPUT_PULLUP);
   pinMode (tank1High, INPUT_PULLUP);
   pinMode (tank1Clean, INPUT_PULLUP);
+  pinMode (tank1Error, OUTPUT);
+  pinMode (tank2Low, INPUT_PULLUP);
+  pinMode (tank2High, INPUT_PULLUP);
+  pinMode (tank2Clean, INPUT_PULLUP);
+  pinMode (tank2Error, OUTPUT);
+  pinMode (tank3Low, INPUT_PULLUP);
+  pinMode (tank3High, INPUT_PULLUP);
+  pinMode (tank3Clean, INPUT_PULLUP);
+  pinMode (tank3Error, OUTPUT);
+  pinMode (tank4Low, INPUT_PULLUP);
+  pinMode (tank4High, INPUT_PULLUP);
+  pinMode (tank4Clean, INPUT_PULLUP);
+  pinMode (tank4Error, OUTPUT);
+
   pinMode (cistern, INPUT_PULLUP);
-  pinMode (pump, OUTPUT);
+  pinMode (pumpControl, OUTPUT);
   pinMode (error, OUTPUT);
   pinMode (eShutoff, INPUT_PULLUP);
   pinMode (manualOveride, INPUT_PULLUP);
+
 
 
   // set the Time library to use Teensy 3.0's RTC to keep time
@@ -54,15 +71,67 @@ void setup() {
 }
 
 void loop() {
-  // bool tank1pumpRun = (digitalRead(tank1Low) ==  openSwitch) && (digitalRead(tank1High) == openSwitch);
-  bool tank1pumpRun = (digitalRead(tank1High) == openSwitch);
-  bool tank1pumpError = (digitalRead(tank1Low) == openSwitch) && (digitalRead(tank1High) == closedSwitch);
+  //tank status in various conditions
+  bool tank1PumpRun = (digitalRead(tank1High) == openSwitch);
+  bool tank1PumpError = (digitalRead(tank1Low) == openSwitch) && (digitalRead(tank1High) == closedSwitch);
   bool tank1Cleaning = (digitalRead(tank1Clean) == closedSwitch);
+
+  bool tank2PumpRun = (digitalRead(tank2High) == openSwitch);
+  bool tank2PumpError = (digitalRead(tank2Low) == openSwitch) && (digitalRead(tank2High) == closedSwitch);
+  bool tank2Cleaning = (digitalRead(tank2Clean) == closedSwitch);
+
+  bool tank3PumpRun = (digitalRead(tank3High) == openSwitch);
+  bool tank3PumpError = (digitalRead(tank3Low) == openSwitch) && (digitalRead(tank3High) == closedSwitch);
+  bool tank3Cleaning = (digitalRead(tank3Clean) == closedSwitch);
+
+  bool tank4PumpRun = (digitalRead(tank4High) == openSwitch);
+  bool tank4PumpError = (digitalRead(tank4Low) == openSwitch) && (digitalRead(tank4High) == closedSwitch);
+  bool tank4Cleaning = (digitalRead(tank4Clean) == closedSwitch);
+
+  //variables for each tank is used in header file for statuses: clean, high, and low states
+  // Change hard coded times to the defined variables
   bool canRunPump = (((hour() >= timepumpStartsHr) && (hour() < timepumpStopsHr)) && waterRun == false);
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (tank1Cleaning) {
+    tank1PumpRun  = true;
+    cleanLogMessage(dataFile, tank1CleanStatus);
+  } else {
+    tank1PumpRun = (digitalRead(tank1High) == openSwitch);
+    cleanRecord1 = 0;
+  }
+
+  if (tank2Cleaning) {
+    tank2PumpRun = true;
+    cleanLogMessage(dataFile, tank2CleanStatus);
+  } else {
+    tank2PumpRun = (digitalRead(tank2High) == openSwitch);
+    cleanRecord2 = 0;
+  }
+
+  if (tank3Cleaning) {
+    tank3PumpRun = true;
+    cleanLogMessage(dataFile, tank3CleanStatus);
+  } else {
+    tank3PumpRun = (digitalRead(tank3High) == openSwitch);
+    cleanRecord3 = 0;
+  }
+
+  if (tank4Cleaning) {
+    tank4PumpRun = true;
+    cleanLogMessage(dataFile, tank4CleanStatus);
+  } else {
+    tank4PumpRun = (digitalRead(tank4High) == openSwitch);
+    cleanRecord4 = 0;
+  }
+  //reference all tanks for code readability
+  bool allTanksPumpRun = (tank1PumpRun && tank2PumpRun && tank3PumpRun && tank4PumpRun);
+  bool allTanksPumpable = (canRunPump && allTanksPumpRun);
 
   //TODO: May consider combining tankCleaning and tankpumpRun
 
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  //Portion of code sets teensy clock to RTC
   if (Serial.available()) {
     time_t t = processSyncMessage();
     if (t != 0) {
@@ -72,7 +141,9 @@ void loop() {
   }
 
 
-  /* Single Tank Test code goes here*/
+
+
+  /* Tank Test code goes here*/
 
   //how the cistern fxns and interacts with the other pumps based on water levels
   //currently it runs when the cistern has water otherwise it does not run
@@ -92,9 +163,17 @@ void loop() {
         pumpDayReset();
 
         // When Low level float sensor is down, turn pump on
-        if (tank1pumpRun && !tank1Cleaning && canRunPump) {
-          digitalWrite(pump, HIGH);
-          digitalWrite(error, LOW); //light off
+        if (allTanksPumpable) {
+          digitalWrite(pumpControl, HIGH);
+          digitalWrite(tank1Error, LOW); //light off
+          digitalWrite(tank2Error, LOW); //light off
+          digitalWrite(tank3Error, LOW); //light off
+          digitalWrite(tank4Error, LOW); //light off
+          errRecord1 = 0;
+          errRecord2 = 0;
+          errRecord3 = 0;
+          errRecord4 = 0;
+
 
           //Data logging once to avoid repetitive messages
           logMessage(dataFile, startPumpMessage);
@@ -105,19 +184,50 @@ void loop() {
             currentDay = day();
           }
         }
-        else if (tank1pumpError && !tank1Cleaning) {
+        else if ((tank1PumpError && !tank1Cleaning) || (tank2PumpError && !tank2Cleaning) ||
+                 (tank3PumpError && !tank3Cleaning) || (tank4PumpError && !tank4Cleaning)) {
           //if low level float sensor is down and high level float sensor is up
           //pump turns off and error light is turned on
-          digitalWrite(pump, LOW); //pump off
-          digitalWrite(error, HIGH); //light on
+          digitalWrite(pumpControl, LOW); //pump off
 
-          //Data logging once to avoid repetitive messages
-          logMessage(dataFile, errorFloatStatus);
+          if (tank1PumpError) {
+            digitalWrite(tank1Error, HIGH); //light on
+            errorLogMessage(dataFile, tank1ErrorFloatStatus);
+
+          }
+
+          if (tank2PumpError) {
+            digitalWrite(tank2Error, HIGH); //light on
+            errorLogMessage(dataFile, tank2ErrorFloatStatus);
+
+          }
+
+          if (tank3PumpError) {
+            digitalWrite(tank3Error, HIGH); //light on
+            errorLogMessage(dataFile, tank3ErrorFloatStatus);
+
+          }
+
+          if (tank4PumpError) {
+            digitalWrite(tank4Error, HIGH); //light on
+            errorLogMessage(dataFile, tank4ErrorFloatStatus);
+
+          }
         }
         else {
           //when high level float sensor is up, turn pump off
-          digitalWrite(pump, LOW); //pump off
-          digitalWrite(error, LOW); //light off
+          digitalWrite(pumpControl, LOW); //pump off
+          digitalWrite(tank1Error, LOW); //light off
+          digitalWrite(tank2Error, LOW); //light off
+          digitalWrite(tank3Error, LOW); //light off
+          digitalWrite(tank4Error, LOW); //light off
+
+          //TODO: errorRecord[respective tank num] that is set equal to one needs
+          //to be moved after the print out statement when fxn is called to avoid multiple print outs
+          errRecord1 = 0;
+          errRecord2 = 0;
+          errRecord3 = 0;
+          errRecord4 = 0;
 
           //Data logging once to avoid repetitive messages
           logMessage(dataFile, Neutral);
@@ -125,8 +235,17 @@ void loop() {
       }
       else {//Manual Else
         if (manualPumpRun == false) {
-          digitalWrite(pump, HIGH);//pump on
-          digitalWrite(error, LOW); //light off
+          digitalWrite(pumpControl, HIGH);//pump on
+          digitalWrite(tank1Error, LOW); //light off
+          digitalWrite(tank2Error, LOW); //light off
+          digitalWrite(tank3Error, LOW); //light off
+          digitalWrite(tank4Error, LOW); //light off
+          //TODO: errorRecord[respective tank num] that is set equal to one needs
+          //to be moved after the print out statement when fxn is called to avoid multiple print outs
+          errRecord1 = 0;
+          errRecord2 = 0;
+          errRecord3 = 0;
+          errRecord4 = 0;
 
           //Data logging once to avoid repetitive messages
           logMessage(dataFile, manualOverideMessage);
@@ -136,20 +255,38 @@ void loop() {
           if (!manualStartPump) {
             manualTimeStart = now();
             manualStartPump = true;
-            currentDay = minute();
+            currentDay = day();
           }
         }
         else {
-          digitalWrite(pump, LOW);//pump off
-          digitalWrite(error, LOW); //light off
+          digitalWrite(pumpControl, LOW);//pump off
+          digitalWrite(tank1Error, LOW); //light off
+          digitalWrite(tank2Error, LOW); //light off
+          digitalWrite(tank3Error, LOW); //light off
+          digitalWrite(tank4Error, LOW); //light off
+          //TODO: errorRecord[respective tank num] that is set equal to one needs
+          //to be moved after the print out statement when fxn is called to avoid multiple print outs
+          errRecord1 = 0;
+          errRecord2 = 0;
+          errRecord3 = 0;
+          errRecord4 = 0;
           logMessage(dataFile, manualPumpDailyRun);
         }
 
       }
     }
     else {//Cistern else
-      digitalWrite(pump, LOW); //pump off
-      digitalWrite(error, LOW); //light off
+      digitalWrite(pumpControl, LOW); //pump off
+      digitalWrite(tank1Error, LOW); //light off
+      digitalWrite(tank2Error, LOW); //light off
+      digitalWrite(tank3Error, LOW); //light off
+      digitalWrite(tank4Error, LOW); //light off
+      //TODO: errorRecord[respective tank num] that is set equal to one needs
+      //to be moved after the print out statement when fxn is called to avoid multiple print outs
+      errRecord1 = 0;
+      errRecord2 = 0;
+      errRecord3 = 0;
+      errRecord4 = 0;
 
       //TODO: Have this not happen when initially running
       //potentially set pump off message to be default message
@@ -158,24 +295,31 @@ void loop() {
     }
   }
   else {//eShutoff else
-    digitalWrite(pump, LOW); //pump off
-    digitalWrite(error, LOW); //light off
+    digitalWrite(pumpControl, LOW); //pump off
+    digitalWrite(tank1Error, LOW); //light off
+    digitalWrite(tank2Error, LOW); //light off
+    digitalWrite(tank3Error, LOW); //light off
+    digitalWrite(tank4Error, LOW); //light off
+    //TODO: errorRecord[respective tank num] that is set equal to one needs
+    //to be moved after the print out statement when fxn is called to avoid multiple print outs
+    errRecord1 = 0;
+    errRecord2 = 0;
+    errRecord3 = 0;
+    errRecord4 = 0;
 
     //Data logging once to avoid repetitive messages
     logMessage(dataFile, eShutoffMessage);
   }
 
-  /*
-    if (waterRun) {
-      //Data logging once to avoid repetitive messages
-      logMessage(dataFile, pumpDailyRun);
-    }
-  */
   if (tank1Cleaning) {
     //Data logging once to avoid repetitive messages
     logMessage(dataFile, cleaningStatus);
   }
-  delay(1000);
+
+  /* For testing purposes this displays time second to second
+      digitalClockDisplay3();
+      delay(1000);
+  */
 
 
   dataFile.close();
@@ -203,7 +347,7 @@ void manualPumpDayReset()
 bool pumptimeRestrict(bool restriction)
 {
   bool itRan = restriction;
-  if ((now() >= (timeStart + howlongAutoPumpRanSec )) && (currentDay == day())) {
+  if ((now() >= (timeStart + howlongAutoPumpRanSec)) && (currentDay == day())) {
     //TODO
     // Change hard coded times to the defined variables
 
@@ -242,8 +386,112 @@ void logMessage(File dataFile, String messageToWrite)
     message = messageToWrite;
     digitalClockDisplay(dataFile);
     dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
   }
 }
+
+//TODO: errorRecord[respective tank num] that is set equal to one needs
+//to be moved after the print out statement when fxn is called to avoid multiple print outs
+void errorLogMessage(File dataFile, String messageToWrite)
+{ //Data logging once to avoid repetitive error messages
+  if (errRecord1 == 0) {
+    errRecord1 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+  if (errRecord2 == 0) {
+    errRecord2 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+  if (errRecord3 == 0) {
+    errRecord3 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+  if (errRecord4 == 0) {
+    errRecord4 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+}
+
+//TODO: cleanRecord[respective tank num] that is set equal to one needs
+//to be moved after the print out statement to avoid double print outs
+void cleanLogMessage(File dataFile, String messageToWrite)
+{ //Data logging once to avoid repetitive clean messages
+  if (cleanRecord1 == 0) {
+    cleanRecord1 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+  if (cleanRecord2 == 0) {
+    cleanRecord2 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+  if (cleanRecord3 == 0) {
+    cleanRecord3 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+
+  if (cleanRecord4 == 0) {
+    cleanRecord4 = 1;
+    message = messageToWrite;
+    digitalClockDisplay(dataFile);
+    dataFile.println(message);
+    //dataFile.close();
+    //Send what we wrote to the Serial Monitor
+    digitalClockDisplay2();
+    Serial.println(message);
+  }
+}
+
 
 
 /*  code to process time sync messages from the serial port   */
@@ -265,11 +513,11 @@ unsigned long processSyncMessage() {
 
 // digital clock display of the time for microSD
 void digitalClockDisplay(File dataFile) {
-  dataFile.print(hour());
+  dataFile.print(second());
   printDigits(minute(), dataFile);
   printDigits(second(), dataFile);
   dataFile.print(" ");
-  dataFile.print(day());
+  dataFile.print(minute());
   dataFile.print(" ");
   dataFile.print(month());
   dataFile.print(" ");
@@ -291,6 +539,37 @@ void printDigits(int digits, File dataFile) {
     dataFile.print('0');
   dataFile.print(digits);
 }
+
+//to delete
+void digitalClockDisplay2() {
+  // digital clock display of the time Serial monitor (mirrors microSd input)
+  Serial.print(hour());
+  printDigits2(minute());
+  printDigits2(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year());
+  Serial.print(" - ");
+}
+
+//for testing, displays time for timing tests
+//For testing purposes this displays time second to second
+/*void digitalClockDisplay3() {
+  //to delete
+  Serial.print(hour());
+  printDigits2(minute());
+  printDigits2(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year());
+  Serial.println();
+  }*/
 
 void printDigits2(int digits) {
   // utility function for digital clock display: prints preceding colon and leading 0
