56574d2cf6bc404d6145e4664eaee936107420b7
curamoto <58994372+curamoto@users.noreply.github.com>
2020-01-14 13:11:55 -0800

    Final code update
    For automated pump control system:
    Main code includes being set to non-test time parameters
    
    Error handling for tanks:
    If all tanks are cleaning don’t pump
    If the system has run for the days allotted time don’t pump
    Datalogging for everything implemented (Error, Cleaning, Pump state, and EOC [eShutoff, manual override, cistern condition])
    
    Task accomplished for tanks:
    Time variables adjusted for testing vs daily application
    LEDs and functionality applied for each tank
    Time resets for each day and based on the mode manual or automatic
    LED error light is reset when there is no longer an error
    pumpLED signal was changed to pumpControl to allow the system to toggle the surge protector on or off
    
    @Neunis

--- definitions.h
+++ definitions.h
@@ -2,9 +2,9 @@
 #define openSwitch true
 
 //tanks and switches are defined below
-#define cistern 25 // Main Cistern Low level float switch
+#define cistern 32 // Main Cistern Low level float switch
 #define error 21 // LED for when an error case is thrown
-#define pumpControl 20 //signal that tells pump to turn on/off
+#define pumpControl 33 //signal that tells pump to turn on/off
 
 #define eShutoff 22
 #define manualOveride 23
@@ -35,15 +35,26 @@
 //MISO,sClock, MOSI are already defined, do nothing
 
 //Single Tank test
-#define pump 13
+#define pumpLED 13
 
 //Time defintions for when pump is active
 #define timepumpStartsHr 10
 #define timepumpStopsHr 16
 #define howlongAutoPumpRanSec 3600
-#define howlongManPumpRanSec 500
+#define howlongManPumpRanSec 7200
 
-String message = "Pump has been turned off";;
+int errRecord1 = 0;
+int errRecord2 = 0;
+int errRecord3 = 0;
+int errRecord4 = 0;
+
+int cleanRecord1 = 0;
+int cleanRecord2 = 0;
+int cleanRecord3 = 0;
+int cleanRecord4 = 0;
+
+
+String message = "Pump has been turned off";
 String startPumpMessage = "Automated pump has started";
 String eShutoffMessage = "Emergency Shutoff switch has been intiated";
 String cisternMessage = "Cistern is low, pump disabled.";
@@ -51,6 +62,14 @@ String manualOverideMessage = "Manual override has been engaged";
 String manualPumpDailyRun = "Manual Pump has been run for the day";
 String pumpOffMessage = "Pump has been turned off";
 String pumpDailyRun = "Pump has run for the day";
-String cleaningStatus = "Pump is being cleaned";
+String cleaningStatus = "Tank is being cleaned";
+String tank1CleanStatus = "Tank 1 is being cleaned";
+String tank2CleanStatus = "Tank 2 is being cleaned";
+String tank3CleanStatus = "Tank 3 is being cleaned";
+String tank4CleanStatus = "Tank 4 is being cleaned";
 String errorFloatStatus = "High float switch is closed, low level float switch is open";
+String tank1ErrorFloatStatus = "Tank 1: High float switch is closed, low level float switch is open";
+String tank2ErrorFloatStatus = "Tank 2: High float switch is closed, low level float switch is open";
+String tank3ErrorFloatStatus = "Tank 3: High float switch is closed, low level float switch is open";
+String tank4ErrorFloatStatus = "Tank 4: High float switch is closed, low level float switch is open";
 String Neutral = "Pump is off, cistern level is met, tanks are full, or time allotted has been met";
